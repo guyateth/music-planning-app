0bd7ad00de3a3f19101e4990931974a08fc33ce3
guyateth <guluethi@student.ethz.ch>
2018-10-29 14:59:01 +0100

    Prototype of the homescreen

---
+++ Code/Styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+const textStyles = StyleSheet.create({
+  title: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    height: 60,
+    backgroundColor: 'gray'
+  },
+  normalText: {
+    height: 60,
+    backgroundColor: 'gray'
+  },
+});
+
+export { textStyles };
